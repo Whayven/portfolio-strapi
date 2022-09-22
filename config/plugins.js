542ca81a1bc5bd564469e5119642c8dd2b35bbd3
Whayven <waynefosterjr@hotmail.com>
2022-09-22 12:17:52 -0400

    updated plugins config with jwt

--- config/plugins.js
+++ config/plugins.js
@@ -9,6 +9,7 @@ module.exports = ({ env }) => ({
         params: {
           Bucket: env('AWS_BUCKET_NAME'),
         },
+        jwtSecret: env('JWT_SECRET')
       },
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
