83f66544259a3ed362e8a225fecec1f841489292
Whayven <waynefosterjr@hotmail.com>
2022-09-22 15:19:30 -0400

    updated s3 plugin config

--- config/plugins.js
+++ config/plugins.js
@@ -14,10 +14,10 @@ module.exports = ({ env }) => ({
       // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
       actionOptions: {
         upload: {
-          ACL: null
+          ACL: "private"
         },
         uploadStream: {
-          ACL: null
+          ACL: "private"
         },
       }
     },
