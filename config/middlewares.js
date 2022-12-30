dc6b04d15ade0dab6ddf8cd4f82ea9b03cb071d8
Whayven <waynefosterjr@hotmail.com>
2022-12-30 13:24:34 -0500

    added articles, about, and projects pages

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,3 @@
-const BUCKET = process.env.AWS_BUCKET_NAME;
-const BUCKET_URL = `https://${BUCKET}.s3.amazonaws.com`;
-
 module.exports = [
   'strapi::errors',
   'strapi::security',
---
+++ src/api/articles-page/routes/articles-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * articles-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::articles-page.articles-page');
