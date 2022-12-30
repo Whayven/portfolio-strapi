b5843f27e834dfc4f13dadcff455c5136771bfdc
Whayven <waynefosterjr@hotmail.com>
2022-12-30 13:32:44 -0500

    removed docker and config files from PR

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,22 @@
+const BUCKET = process.env.AWS_BUCKET_NAME;
+const BUCKET_URL = `https://${BUCKET}.s3.amazonaws.com`;
+
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          'media-src': ["'self'", 'data:', 'blob:', BUCKET_URL],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
